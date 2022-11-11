19cb70a77f4168f06aa422c5f743c0218dbeb8ff
iamrosada <luisrosada2018@gmail.com>
2022-11-12 02:09:53 +0300

    feat: sigin and singIn pages

---
+++ src/app/presentation/pages/SignUp/SignUp.tsx
@@ -0,0 +1,6 @@
+import AuthSignUp from "../../common/components/Authentication/AthSignUp/signup";
+import { SignUpProps } from "./SignUp.types";
+
+export default function SignUp({ storage, addAccount }: SignUpProps) {
+  return <AuthSignUp addAccount={addAccount} storage={storage} />;
+}
