b14429ea37033bf8f154b0de7abe91df1d655142
iamrosada <luisrosada2018@gmail.com>
2022-11-12 02:07:25 +0300

    feat: all usecases for our user

--- src/app/data/usecases/remote-authentication/remote-authentication.ts
+++ src/app/data/usecases/remote-authentication/remote-authentication.ts
@@ -1,29 +1,31 @@
-import { InvalidCredentialsError, UnexpectedError } from 'domain/errors'
-import { HttpClient, HttpStatusCode } from 'data/protocols/http'
-import { Authentication } from 'domain/usecases'
+import { InvalidCredentialsError, UnexpectedError } from "@/app/domain/errors";
+import { HttpClient, HttpStatusCode } from "@/app/data/protocols/http";
+import { Authentication } from "@/app/domain/usecases";
 
 export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
-    private readonly httpClient: HttpClient<RemoteAuthentication.Model>,
+    private readonly httpClient: HttpClient<RemoteAuthentication.Model>
   ) {}
 
   async auth(params: Authentication.Params): Promise<Authentication.Model> {
     const httpResponse = await this.httpClient.request({
       url: this.url,
-      method: 'post',
-      body: params
-    })
-
+      method: "post",
+      body: params,
+    });
+    console.log("AUTH", httpResponse.body);
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.created: return httpResponse.body as Authentication.Model
-      case HttpStatusCode.badRequest: throw new InvalidCredentialsError()
-      default: throw new UnexpectedError()
+      case HttpStatusCode.created:
+        return httpResponse.body as Authentication.Model;
+      case HttpStatusCode.badRequest:
+        throw new InvalidCredentialsError();
+      default:
+        throw new UnexpectedError();
     }
   }
-  
 }
 
 export namespace RemoteAuthentication {
-  export type Model = Authentication.Model
+  export type Model = Authentication.Model;
 }
