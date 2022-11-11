c442a423d24cc8d7a4ed5ad018369f6eaf12bcd3
iamrosada <luisrosada2018@gmail.com>
2022-11-12 02:16:34 +0300

    feat: usecases for page and components

---
+++ src/app/main/factories/pages/makePostListFactory.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { CardCreatePost } from "@/app/presentation/common/components/CardCreatePost";
+import { makeRemoteAddPost } from "../usecases";
+import { makeLocalStorageAdapter } from "../../cache/local-storage-adapter-factory";
+
+export const MakePostsList: React.FC = () => {
+  console.log(makeRemoteAddPost(), "makeRemoteAddPost");
+  return (
+    <CardCreatePost
+      storage={makeLocalStorageAdapter()}
+      addPost={makeRemoteAddPost()}
+    />
+  );
+};
