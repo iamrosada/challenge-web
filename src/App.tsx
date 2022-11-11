ab896226f13d19558c250c4056a4ec26762d03d8
iamrosada <luisrosada2018@gmail.com>
2022-11-12 02:18:23 +0300

    feat: updated the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
-function App() {
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./app/presentation/modules/contexts/auth-context";
+import Background from "./app/presentation/common/components/background/background";
+import Router from "./app/main/routes/router";
+import {
+  theme,
+  themeStyleComponents,
+} from "./app/presentation/styles/global-styles";
+import { ThemeProvider } from "styled-components";
+import PostsProvider from "./app/presentation/modules/contexts/postItem";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <PostsProvider>
+          <ChakraProvider theme={theme}>
+            <ThemeProvider theme={themeStyleComponents}>
+              <Background>
+                <Router />
+              </Background>
+            </ThemeProvider>
+          </ChakraProvider>
+        </PostsProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/app/interfaces/post.interface.ts
@@ -0,0 +1,6 @@
+export interface Postx {
+  id?: string;
+  name: string;
+  message: string;
+  urlx: string;
+}
